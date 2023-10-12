ba3d453c6b37429fe4234f576ffa3a4c3f24e5f7
Anshuman Bhardwaj <64587347+A-nshuman@users.noreply.github.com>
2023-10-12 23:19:02 +0300

    Create pin_guesser.py

---
+++ pin_guesser.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import random
+import time
+
+def generate_pin():
+    pin = random.randint(1000, 9999)
+    return pin
+
+def main():
+    driver = webdriver.Edge(executable_path='C:\')
+
+    driver.get("https://www.guessthepin.com/")
+
+    while True:
+        pin = generate_pin()
+        print(pin)
+
+        input_element = driver.find_element_by_css_selector("#pin-input")
+
+        input_element.send_keys(str(pin))
+
+        input_element.send_keys(Keys.RETURN)
+
+        time.sleep(1)
+
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
